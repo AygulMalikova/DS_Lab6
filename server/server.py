b0d5de2da7e4ff487f7a9b18daa4e6736147a99b
aygulmalikova <amalikova19@gmail.com>
2019-10-02 17:11:02 +0300

    ! Server port

--- server/server.py
+++ server/server.py
@@ -61,7 +61,7 @@ def main():
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-    sock.bind(('', 8000))
+    sock.bind(('', 80))
     sock.listen()
 
     while True:
